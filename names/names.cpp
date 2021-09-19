c9de650286092774a5e26ca32808d6fa4681154f
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 10:25:13 -0800

    placed prototypes needed

--- names/names.cpp
+++ names/names.cpp
@@ -15,5 +15,19 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-   
+    //function prototype to use
+    void InputNames(std::vector<std::string> &names);
 }
+
+
+//write and test a function that searches for a name on the list
+//return a bool value for if the name exists or not
+//use the prototype
+bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names);
+
+//write a function that takes name lits and prints them out
+void PrintNames(const std::vector<std::string>& names);
+
+//write a function taking a list of names and does smething interesting
+//use the function prototype for print names as a basis for this new function
+
