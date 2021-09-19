9e546557801e0a0f7a0f6c5acee348b78a70642b
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 14:36:54 -0800

    tasks are commented

--- rice/rice.cpp
+++ rice/rice.cpp
@@ -2,11 +2,18 @@
 // Lisa Jacklin
 // CS 203
 // 9/18/2021
-//
+//Tasks: 
+//double rice grains per move. for values 1000, 1 mil, and 1 bil
+//answer questions (have them print):
+//1. what is the largest number of squares for which you can calculate the exact 
+//number of grains using an int?
+//2. what is the largest number of squares you can calculate the approximate
+// number of grains using a double?
 
 #include <iostream>
 
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
