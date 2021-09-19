ec22921c4ad221454f79ac8769e3931c925250d7
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-18 18:44:44 -0800

    Add project files.

---
+++ names/names.cpp
@@ -0,0 +1,14 @@
+// names.cpp 
+// Lisa Jacklin
+// Cs 201
+// 9/15/2021
+// Requirements: std::vector class with a variety of loops
+// 
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
