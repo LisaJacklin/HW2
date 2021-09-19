1158b51a9fee03a67641b14952b3e74ef81be307
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 10:42:42 -0800

    text and user in. added

--- money/money.cpp
+++ money/money.cpp
@@ -5,10 +5,31 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int pennies;
+    int nickles;
+    int dimes;
+    int quarters;
+    int largc;
+
+
+    std::cout << "Enter how many coins you have \n";
+    std::cout << "Pennies: ";
+    std::cin >> pennies; "\n";
+    std::cout << "Nickles: ";
+    std::cin >> nickles;
+    std::cout << "dimes: ";
+    std::cin >> dimes;
+    std::cout << "quarters: ";
+    std::cin >> quarters;
+    std::cout << "dollar coins: ";
+    std::cout >> largc;
+
+
+
 }
 //write a program that prompts users to enter coins and 
 //their collective value.
