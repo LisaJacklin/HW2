e914b3f8efcc96b6a6b671d88f3636ca6c810511
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 10:33:08 -0800

    money.cpp created

---
+++ money/money.cpp
@@ -0,0 +1,17 @@
+// money.cpp 
+// Lisa Jacklin
+// CS 203
+// 9/14/2021
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+//write a program that prompts users to enter coins and 
+//their collective value.
+
+//update it once completed to be gramerically correct
+// 1 dime not 1 dimes, etc.
