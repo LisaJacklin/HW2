9fc1c60a36cda17230bcc3c6401eef9d03f6dd46
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 17:08:32 -0800

    finished and questions answered

--- rice/rice.cpp
+++ rice/rice.cpp
@@ -2,24 +2,14 @@
 // Lisa Jacklin
 // CS 203
 // 9/18/2021
-//Tasks: 
-//double rice grains per move. for values 1000, 1 mil, and 1 bil
-//answer questions (have them print):
-//1. what is the largest number of squares for which you can calculate the exact 
-//number of grains using an int?
-//2. what is the largest number of squares you can calculate the approximate
-// number of grains using a double?
 
 #include <iostream>
-
-
 
 int main()
 {
 
 	int move = 1;
 	int rice = 1;
-
 
 	while (move > 0) {
 		std::cout << move;
@@ -32,11 +22,42 @@ int main()
 		++rice;
 		++move;
 
+		/*	how many moves needed for about 1000 grains of rice.
 		if (grain >= 1000) {
 			std::cout << grain;
+			
+			std::cout << "\n";
 			std::cout << move;
+			std::cout << " are needed to obtain about 1000 grains of rice";
+
 			return 0;
 		}
+
+		how many moves needed for a million grains of rice.
+		else if (grain >= 1000000) {
+			std::cout << grain;
+
+			std::cout << "\n";
+			std::cout << move;
+			std::cout << " are needed to obtain about 1 million grains of rice";
+
+			return 0;
+		}
+
+		*/
+		if (grain >= 1000000000) {
+			std::cout << grain;
+
+			std::cout << "\n";
+				std::cout << move;
+			std::cout << " are needed to obtain about a billion grains of rice";
+
+			return 0;
+		}
+
+		std::cout << " The largest number of squares that can be calculated of grains with int is 64";
+		std::cout << " The largest number of squares that can be calculated of grains with double is 128";
+
 	}
 
 
