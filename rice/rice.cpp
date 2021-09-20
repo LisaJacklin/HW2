af5623eafe162fe786a3edbc970e8fa51f990bc1
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 19:04:44 -0800

    names are printed and accepted.

--- rice/rice.cpp
+++ rice/rice.cpp
@@ -22,16 +22,18 @@ int main()
 		++rice;
 		++move;
 
-		/*	how many moves needed for about 1000 grains of rice.
+		//how many moves needed for about 1000 grains of rice.
+	
 		if (grain >= 1000) {
 			std::cout << grain;
-			
+
 			std::cout << "\n";
 			std::cout << move;
 			std::cout << " are needed to obtain about 1000 grains of rice";
 
 			return 0;
 		}
+		/*	
 
 		how many moves needed for a million grains of rice.
 		else if (grain >= 1000000) {
@@ -43,9 +45,7 @@ int main()
 
 			return 0;
 		}
-
-		*/
-		if (grain >= 1000000000) {
+			if (grain >= 1000000000) {
 			std::cout << grain;
 
 			std::cout << "\n";
@@ -53,12 +53,13 @@ int main()
 			std::cout << " are needed to obtain about a billion grains of rice";
 
 			return 0;
+		*/
+	
 		}
 
-		std::cout << " The largest number of squares that can be calculated of grains with int is 64";
-		std::cout << " The largest number of squares that can be calculated of grains with double is 128";
+		std::cout << " The largest number of squares that can be calculated of grains with int is 64\n";
+		std::cout << " The largest number of squares that can be calculated of grains with double is 128\n";
 
 	}
 
 
-}
