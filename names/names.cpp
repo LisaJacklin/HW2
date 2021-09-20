62b22aec238ee228fcbad3b5e95a6309ffd85ca1
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-19 20:05:49 -0800

    void input name set up. struggling with main loop

--- names/names.cpp
+++ names/names.cpp
@@ -10,29 +10,44 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include "names.h"
+
+   //function prototype to use
+
+void InputNames(std::vector<std::string>& names);
 
 int main()
 {
 
-    //function prototype to use
-    void InputNames(std::vector<std::string> &names);
+}
+ 
+void InputNames(std::vector<std::string>& names) {
 
     for (int i = 0; i < 10; i++) {
         std::string name;
         std::cout << "Please enter a name: ";
-        std::cin >> name; 
+        std::cin >> name;
         std::cout << "\n";
     }
-    return 0;
-
 
 }
 
+    //takes name list and prints
+    void PrintNames(const std::vector<std::string>&names);
+
+
+
+//remember bool has two values, true or false
+bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names);
+  
+
+
+    
 
 //write and test a function that searches for a name on the list
 //return a bool value for if the name exists or not
 //use the prototype
-//bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names);
+
 
 //write a function that takes name lits and prints them out
 //void PrintNames(const std::vector<std::string>& names);
